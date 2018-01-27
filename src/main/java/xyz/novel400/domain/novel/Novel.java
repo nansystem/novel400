162edc2a27a7c165abab41d408f49a5467702220
satoshi-n <sato171786@gmail.com>
2018-01-27 16:18:45 +0900

    タイトル追加

--- src/main/java/xyz/novel400/domain/novel/Novel.java
+++ src/main/java/xyz/novel400/domain/novel/Novel.java
@@ -2,9 +2,14 @@ package xyz.novel400.domain.novel;
 
 public class Novel {
     NovelIdentifier novelIdentifier;
+    NovelTitle novelTitle;
     NovelText novelText;
 
     Novel() {}
+
+    public NovelTitle title() {
+        return novelTitle;
+    }
 
     public NovelText text() {
         return novelText;
---
+++ src/main/java/xyz/novel400/domain/novel/NovelTitle.java
@@ -0,0 +1,10 @@
+package xyz.novel400.domain.novel;
+
+public class NovelTitle {
+    String value;
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
