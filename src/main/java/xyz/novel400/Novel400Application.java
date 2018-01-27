b37abda12abeac0d6e6ccaa2dadee3650f27f79c
satoshi-n <sato171786@gmail.com>
2018-01-27 13:46:05 +0900

    first commit

---
+++ src/main/java/xyz/novel400/Novel400Application.java
@@ -0,0 +1,12 @@
+package xyz.novel400;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Novel400Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Novel400Application.class, args);
+	}
+}
